ecbea5f0c95c45e3784c948d921fa797184a6313
Albert Essilfie <50079783+Otoole311@users.noreply.github.com>
2022-10-03 15:41:04 +0000

    Initial Commit

---
+++ CurrencyCalculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CurrencyCalculator
+//
+//  Created by Albert Essilfie on 10/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyCalculator/CurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalculatorApp.swift
+//  CurrencyCalculator
+//
+//  Created by Albert Essilfie on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
